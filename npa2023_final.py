17ca66f41a508939c35ab0aca97eefdd2402d908
sStoowie <pronsek007@gmail.com>
2024-10-24 08:33:48 +0000

    commit if webex connected

--- npa2023_final.py
+++ npa2023_final.py
@@ -6,19 +6,22 @@
 #######################################################################################
 # 1. Import libraries for API requests, JSON formatting, time, and (restconf_final or netconf_final).
 
-<!!!REPLACEME with code for libraries>
+import json
+import requests
+import time
+# import restconf_final
 
 #######################################################################################
 # 2. Assign the Webex hard-coded access token to the variable accessToken.
 
-accessToken = "Bearer <!!!REPLACEME with hard-coded token!!!>"
+accessToken = "Bearer YzFkMDNlZmQtYTgxMS00YzM2LTg1NDMtMGEzOTY5OTk1Mjc5MzdiYjM0MjEtZWU0_P0A1_11bad644-5f58-4567-ac3d-807e84a5c47d"
 
 #######################################################################################
 # 3. Prepare parameters get the latest message for messages API.
 
 # Defines a variable that will hold the roomId
 roomIdToGetMessages = (
-    "<!!!REPLACEME with roomID of the NPA2023 Webex Teams room!!!>"
+    "Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vOTIzM2Y3NDAtNWViNy0xMWVmLTlhZTgtODlhNWFjYWQ3MjBl"
 )
 
 while True:
@@ -31,7 +34,7 @@ while True:
     getParameters = {"roomId": roomIdToGetMessages, "max": 1}
 
     # the Webex Teams HTTP header, including the Authoriztion
-    getHTTPHeader = {"Authorization": <!!!REPLACEME!!!>}
+    getHTTPHeader = {"Authorization": accessToken}
 
 # 4. Provide the URL to the Webex Teams messages API, and extract location from the received message.
     
@@ -39,9 +42,9 @@ while True:
     # - Use the GetParameters to get only the latest message.
     # - Store the message in the "r" variable.
     r = requests.get(
-        "<!!!REPLACEME with URL of Webex Teams Messages API!!!>",
-        params=<!!!REPLACEME with HTTP parameters!!!>,
-        headers=<!!!REPLACEME with HTTP headers!!!>,
+        "https://webexapis.com/v1/messages",
+        params=getParameters,
+        headers=getHTTPHeader,
     )
     # verify if the retuned HTTP status code is 200/OK
     if not r.status_code == 200:
@@ -63,46 +66,46 @@ while True:
     message = messages[0]["text"]
     print("Received message: " + message)
 
-    # check if the text of the message starts with the magic character "/" followed by your studentID and a space and followed by a command name
-    #  e.g.  "/66070123 create"
-    if message.find("<!!!REPLACEME!!!>") == 0:
+#     # check if the text of the message starts with the magic character "/" followed by your studentID and a space and followed by a command name
+#     #  e.g.  "/66070123 create"
+#     if message.find("<!!!REPLACEME!!!>") == 0:
 
-        # extract the command
-        command = <!!!REPLACEME!!!>
-        print(command)
+#         # extract the command
+#         command = <!!!REPLACEME!!!>
+#         print(command)
 
-# 5. Complete the logic for each command
+# # 5. Complete the logic for each command
 
-        if command == "create":
-            <!!!REPLACEME with code for create command!!!>     
-        elif command == "delete":
-            <!!!REPLACEME with code for delete command!!!>
-        elif command == "enable":
-            <!!!REPLACEME with code for enable command!!!>
-        elif command == "disable":
-            <!!!REPLACEME with code for disable command!!!>
-        elif command == "status":
-            <!!!REPLACEME with code for status command!!!>
-        else:
-            responseMessage = "Error: No command or unknown command"
+#         if command == "create":
+#             <!!!REPLACEME with code for create command!!!>     
+#         elif command == "delete":
+#             <!!!REPLACEME with code for delete command!!!>
+#         elif command == "enable":
+#             <!!!REPLACEME with code for enable command!!!>
+#         elif command == "disable":
+#             <!!!REPLACEME with code for disable command!!!>
+#         elif command == "status":
+#             <!!!REPLACEME with code for status command!!!>
+#         else:
+#             responseMessage = "Error: No command or unknown command"
         
-# 6. Complete the code to post the message to the Webex Teams room.
+# # 6. Complete the code to post the message to the Webex Teams room.
         
-        # the Webex Teams HTTP headers, including the Authoriztion and Content-Type
-        postHTTPHeaders = HTTPHeaders = {"Authorization": <!!!REPLACEME!!!>, "Content-Type": <!!!REPLACEME!!!>}
+#         # the Webex Teams HTTP headers, including the Authoriztion and Content-Type
+#         postHTTPHeaders = HTTPHeaders = {"Authorization": <!!!REPLACEME!!!>, "Content-Type": <!!!REPLACEME!!!>}
 
-        # The Webex Teams POST JSON data
-        # - "roomId" is is ID of the selected room
-        # - "text": is the responseMessage assembled above
-        postData = {"roomId": <!!!REPLACEME!!!>, "text": <!!!REPLACEME!!!>}
+#         # The Webex Teams POST JSON data
+#         # - "roomId" is is ID of the selected room
+#         # - "text": is the responseMessage assembled above
+#         postData = {"roomId": <!!!REPLACEME!!!>, "text": <!!!REPLACEME!!!>}
 
-        # Post the call to the Webex Teams message API.
-        r = requests.post(
-            "<!!!REPLACEME with URL of Webex Teams Messages API!!!>",
-            data=json.dumps(<!!!REPLACEME!!!>a),
-            headers=<!!!REPLACEME!!!>,
-        )
-        if not r.status_code == 200:
-            raise Exception(
-                "Incorrect reply from Webex Teams API. Status code: {}".format(r.status_code)
-            )
+#         # Post the call to the Webex Teams message API.
+#         r = requests.post(
+#             "<!!!REPLACEME with URL of Webex Teams Messages API!!!>",
+#             data=json.dumps(<!!!REPLACEME!!!>a),
+#             headers=<!!!REPLACEME!!!>,
+#         )
+#         if not r.status_code == 200:
+#             raise Exception(
+#                 "Incorrect reply from Webex Teams API. Status code: {}".format(r.status_code)
+#             )
